688ea9a48582cba65b5fd242d2094c68c3d568ba
chetanbatra94 <chetan.batra94@gmail.com>
2019-07-03 10:15:55 +0530

    VO files commit

---
+++ src/main/java/com/yodlee/docdownloadandtsd/VO/DataDiffDetailsVO.java
@@ -0,0 +1,50 @@
+package com.yodlee.docdownloadandtsd.VO;
+
+public class DataDiffDetailsVO {
+
+    private String sumInfoId;
+    private String dataDiff;
+    private String migratedBy;
+    private String requestedDate;
+    private String migId;
+
+    public String getSumInfoId() {
+        return sumInfoId;
+    }
+
+    public void setSumInfoId(String sumInfoId) {
+        this.sumInfoId = sumInfoId;
+    }
+
+    public String getDataDiff() {
+        return dataDiff;
+    }
+
+    public void setDataDiff(String dataDiff) {
+        this.dataDiff = dataDiff;
+    }
+
+    public String getMigratedBy() {
+        return migratedBy;
+    }
+
+    public void setMigratedBy(String migratedBy) {
+        this.migratedBy = migratedBy;
+    }
+
+    public String getRequestedDate() {
+        return requestedDate;
+    }
+
+    public void setRequestedDate(String requestedDate) {
+        this.requestedDate = requestedDate;
+    }
+
+    public String getMigId() {
+        return migId;
+    }
+
+    public void setMigId(String migId) {
+        this.migId = migId;
+    }
+}
---
+++ src/main/java/com/yodlee/docdownloadandtsd/VO/DocDownloadVO.java
@@ -0,0 +1,59 @@
+package com.yodlee.docdownloadandtsd.VO;
+
+public class DocDownloadVO {
+
+    private String migId;
+    private String migratedBy;
+    private String docDownloadSeed;
+    private String docDownloadProd;
+    private String requestedDate;
+    private String sumInfoId;
+
+    public String getSumInfoId() {
+        return sumInfoId;
+    }
+
+    public void setSumInfoId(String sumInfoId) {
+        this.sumInfoId = sumInfoId;
+    }
+
+    public String getMigId() {
+        return migId;
+    }
+
+    public void setMigId(String migId) {
+        this.migId = migId;
+    }
+
+    public String getMigratedBy() {
+        return migratedBy;
+    }
+
+    public void setMigratedBy(String migratedBy) {
+        this.migratedBy = migratedBy;
+    }
+
+    public String getDocDownloadSeed() {
+        return docDownloadSeed;
+    }
+
+    public void setDocDownloadSeed(String docDownloadSeed) {
+        this.docDownloadSeed = docDownloadSeed;
+    }
+
+    public String getDocDownloadProd() {
+        return docDownloadProd;
+    }
+
+    public void setDocDownloadProd(String docDownloadProd) {
+        this.docDownloadProd = docDownloadProd;
+    }
+
+    public String getRequestedDate() {
+        return requestedDate;
+    }
+
+    public void setRequestedDate(String requestedDate) {
+        this.requestedDate = requestedDate;
+    }
+}
